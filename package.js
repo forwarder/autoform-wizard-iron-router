bafc632df25b4c7089fc3ee4be1f2d3b1d664d4c
Eelco Wiersma <eelco@pagebakers.nl>
2015-10-27 21:24:21 +0100

    Version bump

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'forwarder:autoform-wizard-iron-router',
   summary: 'Iron Router bindings for AutoForm Wizard.',
-  version: '0.1.2',
+  version: '0.1.4',
   git: 'https://github.com/forwarder/meteor-wizard-iron-router.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  
+
   api.use([
-    'forwarder:autoform-wizard@0.7.0',
+    'forwarder:autoform-wizard@0.9.0',
     'iron:router@1.0.0'
   ], 'client');
-  
+
   api.addFiles([
     'client.js'
   ], 'client');
